7647db9f0bf2b011aec366e6670dcf80647f2b7f
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-04-28 16:49:03 +0500

    Добавил файл в settings.py AUTH_USER_MODEL,и реализовал модель User и применил миграции к модели User

--- cinemaapp/accounts/models.py
+++ cinemaapp/accounts/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class RoleChoices(models.TextChoices):
+        USER = "U", "Пользователь"
+        ADMIN = "A", "Администратор"
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        unique=True,
+        blank=False,
+        null=False
+    )
+    role = models.CharField(
+        max_length=10,
+        choices=RoleChoices.choices,
+        blank=True,
+        null=True
+    )
+
+
+    def __str__(self):
+        return self.username
