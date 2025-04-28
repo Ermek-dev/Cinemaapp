5c5fabcf2f5defe30e3d271d839acde6600ecb2c
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-04-28 19:47:18 +0500

    Добавил 'movies/<int:pk>/delete/ для удаление фильма (только для админа).

--- cinemaapp/movies/urls.py
+++ cinemaapp/movies/urls.py
@@ -6,8 +6,5 @@ urlpatterns = [
     path('movies/<int:pk>/',MovieDetailView.as_view(),name='movie_detail'),
     path('movies/create/', MovieCreateView.as_view(), name='movie_create'),
     path('movies/<int:pk>/update/',MovieUpdateView.as_view(),name='movie_update'),
-
-
-
-
+    path('movies/<int:pk>/delete/',MovieUpdateView.as_view(),name='movie_delete'),
 ]
