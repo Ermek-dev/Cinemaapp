e647a54153c513d1ecede2264b0bd06dbb9793d3
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-04-28 16:52:00 +0500

    реализовал модель Movie и применил миграции к модели Movie

--- cinemaapp/movies/models.py
+++ cinemaapp/movies/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(
+        max_length=200,
+        null = False,
+        blank = False,
+        verbose_name = 'Заголовок',
+    )
+    description = models.TextField(
+        max_length=3000,
+        null=False,
+        blank=False,
+        verbose_name='Описание'
+    )
+    duration = models.PositiveIntegerField(
+        default=0
+    )
+    posterUrl = models.URLField()
+    createdAt = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+    def __str__(self):
+        return self.title
