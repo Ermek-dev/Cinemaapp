7d8eea93cb6bb758b1c2d90d50e5dd6ff5a1310b
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-04-28 19:41:58 +0500

    Добавил маршрут 'movies/create/' для создания нового фильма.

--- cinemaapp/movies/urls.py
+++ cinemaapp/movies/urls.py
@@ -4,6 +4,8 @@ from .views import MovieListView, MovieDetailView, MovieCreateView
 urlpatterns = [
     path('movies/',MovieListView.as_view(),name='movie_list'),
     path('movies/<int:pk>/',MovieDetailView.as_view(),name='movie_detail'),
+    path('movies/create/', MovieCreateView.as_view(), name='movie_create'),
+
 
 
 
