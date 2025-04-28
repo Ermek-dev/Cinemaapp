9581b7b72534a9f8f1c3ebdabfd15ab6afda321d
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-04-28 19:36:40 +0500

    создал MovieListView(APIView) для получения списка всех фильмов

--- cinemaapp/movies/views.py
+++ cinemaapp/movies/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic.base import View
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Movie
+from .serializers import MovieSerializer
 
-# Create your views here.
+
+class MovieListView(APIView):
+    def get(self,request, *args, **kwargs):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+
+
+
+
+
+
