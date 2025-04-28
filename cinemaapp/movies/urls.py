1ecd1a1a8de0a267620b1b6af1a8ade5da7390aa
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-04-28 19:39:01 +0500

    создал маршрут 'movies/<int:pk>/' для получения информации о фильме по ID.

--- cinemaapp/movies/urls.py
+++ cinemaapp/movies/urls.py
@@ -3,6 +3,7 @@ from .views import MovieListView, MovieDetailView, MovieCreateView
 
 urlpatterns = [
     path('movies/',MovieListView.as_view(),name='movie_list'),
+    path('movies/<int:pk>/',MovieDetailView.as_view(),name='movie_detail'),
 
 
 
