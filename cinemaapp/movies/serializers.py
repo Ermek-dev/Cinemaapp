411edc46f0b4b839c623e73cf2cc6a6126ee46d3
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-05-01 16:51:14 +0500

    Реализовал валидацию для title,duration,PosterURL

--- cinemaapp/movies/serializers.py
+++ cinemaapp/movies/serializers.py
@@ -1,4 +1,7 @@
+from django.core.validators import URLValidator
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import Movie
 
 
@@ -7,3 +10,25 @@ class MovieSerializer(serializers.ModelSerializer):
         model = Movie
         fields = ('id','title','description','duration','posterUrl','createdAt')
         read_only_fields = ('id','createdAt')
+
+
+    def validate_title(self, value):
+        if not value:
+            raise serializers.ValidationError("Название фильма не может быть пустым")
+        return value
+
+
+    def validate_duration(self, value):
+        if value <=0:
+            raise serializers.ValidationError("Продолжительность фильма не может быть больше 0 минут")
+        return value
+
+
+    def validate_posterUrl(self,value):
+        validator = URLValidator()
+        try:
+            validator(value)
+        except ValidationError:
+            raise serializers.ValidationError("Пожалуйста введите корректный URL адрес")
+        return value
+
