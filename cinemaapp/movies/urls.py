4bd50599634f730bfd3c0db1c34c4c7a3df41c3f
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-05-01 16:53:35 +0500

    Настроил маршруты для CRUD,добавил соответствующие path для movie-(list,detail,create,update,delete)

--- cinemaapp/movies/urls.py
+++ cinemaapp/movies/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import MovieListView, MovieDetailView, MovieCreateView, MovieUpdateView
+from .views import MovieListView, MovieDetailView, MovieCreateView, MovieUpdateView, MovieDeleteView
 
 urlpatterns = [
     path('movies/',MovieListView.as_view(),name='movie_list'),
     path('movies/<int:pk>/',MovieDetailView.as_view(),name='movie_detail'),
     path('movies/create/', MovieCreateView.as_view(), name='movie_create'),
     path('movies/<int:pk>/update/',MovieUpdateView.as_view(),name='movie_update'),
-    path('movies/<int:pk>/delete/',MovieUpdateView.as_view(),name='movie_delete'),
+    path('movies/<int:pk>/delete/',MovieDeleteView.as_view(),name='movie_delete'),
 ]
