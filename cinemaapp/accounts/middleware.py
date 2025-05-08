c3b6c2573b13a345135ec5ca3b3df2b0ae79ae5d
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-05-08 15:45:10 +0500

    Создан middleware для проверки JWT и ролей по маршрутам Добавлена логика исключения /admin из проверки. Проверяется роль пользователя по путям в ROLE_PROTECTED_PATHS. При отсутствии доступа возвращается 403 Access denied.

---
+++ cinemaapp/accounts/middleware.py
@@ -0,0 +1,32 @@
+from django.http import JsonResponse
+from django.utils.deprecation import MiddlewareMixin
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.contrib.auth.models import AnonymousUser
+
+
+ROLE_PROTECTED_PATHS = {
+    'A': ['/admin-only/'],
+    'U': ['/user-only/']
+}
+
+
+class JWTAuthenticationMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        if request.path.startswith('/admin'):
+            return
+        jwt_authenticator = JWTAuthentication()
+        try:
+            user_auth_tuple = jwt_authenticator.authenticate(request)
+            if user_auth_tuple is not None:
+                request.user, _ = user_auth_tuple
+                user_role = getattr(request.user, 'role', None)
+                for role, paths in ROLE_PROTECTED_PATHS.items():
+                    if any(request.path.startswith(p) for p in paths):
+                        if user_role != role:
+                            return JsonResponse(
+                            {'detail': 'Access denied: insufficient role'}, status=403
+                            )
+            else:
+                request.user = AnonymousUser()
+        except Exception as e:
+            request.user = AnonymousUser()
