9809ef019eea417e9eeed110155b9c1c4c143df5
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-05-11 15:36:11 +0500

    хотел добавить  функцию ready в apps.py,потом решил ,что рановато

--- cinemaapp/showtimes/apps.py
+++ cinemaapp/showtimes/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
-class SessionsConfig(AppConfig):
+class ShowTimesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'showtimes'
+
+
+    def ready(self):
+        import showtimes.models
