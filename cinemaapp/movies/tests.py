6516a2d78705b755333e6336d7b31ca439ae1781
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-05-23 03:50:01 +0500

    ✅ Реализовал поиск фильмов по названию + добавлены тесты (MovieSearchTests)
    - Добавлил поддержку query-параметра `search` в MovieListView
    - Поиск осуществляется по частичному совпадению названия (title__icontains)
    - Написаны тесты:
      - успешный поиск по существующему заголовку
      - корректная обработка случая, когда ничего не найдено
    - Соответствие ТЗ: GET /movies?search=<query>

--- cinemaapp/movies/tests.py
+++ cinemaapp/movies/tests.py
@@ -1,6 +1,11 @@
+from rest_framework import status
+from rest_framework.reverse import reverse
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.test import TestCase
 from rest_framework.test import APIClient
+from accounts.models import User
+from .models import Movie
+
 
 class MovieAPITestCase(TestCase):
     def setUp(self):
@@ -25,3 +30,24 @@ class MovieAPITestCase(TestCase):
             duration=148,
             posterUrl="http://example.com/inception.jpg"
         )
+        self.movie2 = Movie.objects.create(
+            title = "Batman Begins",
+            description = "The origin of Batman",
+            duration = 140,
+            posterUrl = "http://example.com/batman.jpg",
+        )
+
+
+    def test_search_movie_by_title(self):
+        url = reverse('movie_list') + '?search=Batman'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(any('Batman' in movie['title'] for movie in response.data))
+
+
+    def test_search_movie_empty_result(self):
+        url = reverse('movie_list') + '?search=NoSuchMovieTitle'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
