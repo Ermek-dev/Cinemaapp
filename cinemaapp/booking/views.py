98bcb41e3bab998ddc1be381412680ddd3096d91
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-06-09 17:06:27 +0500

    Созданы view ,для работы с бронированиями,GET /bookings — получить список бронирований текущего пользователя,GET /sessions/:id/seats — получить занятые места сеанса,POST /bookings — создать новое бронирование (только для пользователя)

--- cinemaapp/booking/views.py
+++ cinemaapp/booking/views.py
@@ -1,3 +1,84 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Booking
+from .serializers import BookingSerializer
+from accounts.permissions import IsAdmin
+from .models import Session
 
-# Create your views here.
+
+class BookingListView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request, *args, **kwargs):
+        bookings = Booking.objects.filter(user=request.user)
+        serializer = BookingSerializer(bookings, many=True)
+        return Response(serializer.data)
+
+
+class TakenSeatsView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request,session_id,*args,**kwargs):
+        try:
+            session = Session.objects.get(id=session_id)
+        except Session.DoesNotExist:
+            return Response({"error": "Сеанс не найден!!!"},status=status.HTTP_404_NOT_FOUND)
+
+
+        bookings = Booking.objects.filter(session=session)
+        taken_seats = []
+        for booking in bookings:
+            taken_seats.extend(booking.seats)
+
+
+        return Response(
+            {
+                "sessionId": session.id,
+                "takenSeats": taken_seats
+            }, status = status.HTTP_200_OK
+        )
+
+
+
+class BookingCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request, *args, **kwargs):
+        session_id = request.data.get("sessionId")
+        seats = request.data.get("seats")
+
+        if not session_id or not seats:
+            return Response({"error": "sessionId и seats обязательны"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Проверка на существование сеанса
+        try:
+            session = Session.objects.get(id=session_id)
+        except Session.DoesNotExist:
+            return Response({"error": "Сеанс не найден"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Проверка занятых мест
+        taken_seats = Booking.objects.filter(session=session).values_list("seats", flat=True)
+        occupied = []
+        for seat in seats:
+            for taken in taken_seats:
+                if seat in taken:
+                    occupied.append(seat)
+
+        if occupied:
+            first = occupied[0]
+            return Response(
+                {"error": f"Место row={first['row']}, seat={first['seat']} уже занято"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Всё свободно — создаём бронирование
+        booking = Booking.objects.create(
+            user=request.user,
+            session=session,
+            seats=seats
+        )
+        return Response(
+            {"message": "Бронирование успешно создано", "bookingId": booking.id},
+            status=status.HTTP_201_CREATED
+        )
+
