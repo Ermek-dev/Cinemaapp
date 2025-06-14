bdd00227c656baf71998ad829f9aac4cc94a6506
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-06-14 17:37:44 +0500

    Обновлён сериализатор BookingSerializer: поле session → sessionId

--- cinemaapp/booking/serializers.py
+++ cinemaapp/booking/serializers.py
@@ -3,13 +3,14 @@ from .models import Booking
 
 
 class BookingSerializer(serializers.ModelSerializer):
+    sessionId = serializers.IntegerField(source='session.id',read_only=True)
     class Meta:
         model = Booking
-        fields = ['id', 'user', 'session','seats','createdAt']
+        fields = ['id', 'user', 'sessionId','seats','createdAt']
 
     def validate_seats(self, value):
         if not isinstance(value, list):
             raise serializers.ValidationError("Поле 'seats' должно быть списком!!!")
         for seat in value:
-            if not isinstance(seat, dict) or 'rows' not in seat or 'seat' not in seat:
+            if not isinstance(seat, dict) or 'row' not in seat or 'seat' not in seat:
                 raise serializers.ValidationError("Каждое место должно быть словарем с ключами 'rows' и 'seat'")
