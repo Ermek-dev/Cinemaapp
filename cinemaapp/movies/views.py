96f41b25310fb6c435205a7e2d6b4cc572e4e18c
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-06-15 16:00:35 +0500

    Добавил in-memory кэширование GET /movies с TTL=60 сек и сбросом при изменении

--- cinemaapp/movies/views.py
+++ cinemaapp/movies/views.py
@@ -1,24 +1,43 @@
 from rest_framework import status
-from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Movie
 from .serializers import MovieSerializer
 from accounts.permissions import IsAdmin
+import time
+
+
+CACHE_TTL = 60
+movie_cache = {}
+
+
+class MovieListView(APIView):
+    def get(self, request, *args, **kwargs):
+        search = request.query_params.get("search", "").strip()
+
+        if len(search)>100:
+            return Response({"error": "Слишком длинный поисковый запрос"})
+
+        cache_key = search.lower()
+        now = time.time()
+
+        if cache_key in movie_cache:
+            cached = movie_cache[cache_key]
+            if now - cached["timestamp"] < CACHE_TTL:
+                return Response(cached["data"], status=status.HTTP_200_OK)
 
 
 
-
-class MovieListView(ListAPIView):
-    serializer_class = MovieSerializer
-
-    def get_queryset(self):
         queryset = Movie.objects.all()
-        search = self.request.query_params.get('search')
         if search and len(search) <= 100:
             queryset = queryset.filter(title__icontains=search)
-        return queryset
+        serializer = MovieSerializer(queryset, many=True)
+        movie_cache[cache_key] = {
+            "data": serializer.data,
+            "timestamp": now
+        }
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class MovieDetailView(APIView):
@@ -36,6 +55,7 @@ class MovieCreateView(APIView):
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             movie = serializer.save()
+            movie_cache.clear()
             return Response({'message': 'Фильм успешно создан','id': movie.id}, status=status.HTTP_201_CREATED)
         return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -53,6 +73,7 @@ class MovieUpdateView(APIView):
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
+            movie_cache.clear()
             return Response({'message': 'Фильм успешно обновлен',})
         return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -68,4 +89,5 @@ class MovieDeleteView(APIView):
             return Response({'error': 'Фильм не найден'}, status=status.HTTP_404_NOT_FOUND)
 
         movie.delete()
+        movie_cache.clear()
         return Response({'message': 'Фильм успешно удален',}, status=status.HTTP_204_NO_CONTENT)
