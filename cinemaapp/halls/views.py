6e940b5d66d6fe0e4212e9f6d2b81480249ad60c
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-06-17 18:51:15 +0500

    добавлены логгирование и централизованная обработка ошибок
    - Централизованная обработка ошибок через raise Exception и Http404
    - Добавлено логгирование при создании, обновлении и удалении залов
    - Добавлены права доступа IsAdmin и JWT аутентификация на защищённые методы

--- cinemaapp/halls/views.py
+++ cinemaapp/halls/views.py
@@ -1,3 +1,4 @@
+import logging
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,65 +6,77 @@ from .models import Hall
 from .serializers import HallSerializer
 from accounts.permissions import IsAdmin
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.http import Http404
 
 
-# Create your views here.
+logger = logging.getLogger('cinemaapp')
+
+
 class HallListView(APIView):
     def get(self, request, *args, **kwargs):
         halls = Hall.objects.all()
         serializer = HallSerializer(halls, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
 
 class HallDetailView(APIView):
     def get(self,request,pk,*args,**kwargs):
         try:
             hall = Hall.objects.get(pk=pk)
-            serializer = HallSerializer(hall)
-            return Response(serializer.data, status=status.HTTP_200_OK)
         except Hall.DoesNotExist:
-            return Response({'error': 'Зал не найден'}, status=status.HTTP_404_NOT_FOUND)
+            raise Http404("Зал не найден")
+
+        serializer = HallSerializer(hall)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class HallCreateView(APIView):
     permission_classes = [IsAdmin]
 
-
     def post(self, request, *args, **kwargs):
         serializer = HallSerializer(data=request.data)
-        if serializer.is_valid():
-            hall = serializer.save()
-            return Response({'message': 'Зал успешно создан', 'id':hall.id}, status=status.HTTP_201_CREATED)
-        return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        hall = serializer.save()
+        logger.info(f"Зал создан: ID={hall.id}")
+        return Response(
+            {'message': 'Зал успешно создан', 'id':hall.id},
+            status=status.HTTP_201_CREATED
+        )
+
 
 
 class HallUpdateView(APIView):
     permission_classes =  [IsAdmin]
     authentication_classes = [JWTAuthentication]
 
+
     def put(self, request,pk,*args,**kwargs):
         try:
             hall = Hall.objects.get(pk=pk)
         except Hall.DoesNotExist:
-            return Response({'error': 'Зал не найден'}, status=status.HTTP_404_NOT_FOUND)
-
+            raise Exception("Зал не найден")
 
         serializer = HallSerializer(hall, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'message': 'Информация о зале обновлена'})
-        return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        logger.info(f"Зал обновлен: ID={hall.id}")
+        return Response({'message': 'Информация о зале обновлена'})
 
 
 class HallDeleteView(APIView):
     permission_classes = [IsAdmin]
     authentication_classes = [JWTAuthentication]
 
+
     def delete(self, request,pk,*args,**kwargs):
         try:
             hall = Hall.objects.get(pk=pk)
         except Hall.DoesNotExist:
-            return Response({'error': 'Зал не найден'}, status=status.HTTP_404_NOT_FOUND)
-
+            raise Exception("Зал не найден")
         hall.delete()
-        return Response({'message': 'Зал успешно удален'}, status=status.HTTP_204_NO_CONTENT)
+        logger.info(f"Зал удален: ID={pk}")
+        return Response(
+            {'message':'Зал успешно удален'},
+            status=status.HTTP_204_NO_CONTENT
+        )
