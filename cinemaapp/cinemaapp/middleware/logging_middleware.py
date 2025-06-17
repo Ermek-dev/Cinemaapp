f5e68aaa59b45d609e9d633a55ccf7840d067a3f
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-06-17 18:44:01 +0500

    добавлен LoggingMiddleware для логирования запросов и централизованной обработки ошибок
    - Логируются все запросы и ответы (метод, путь, статус, длительность)
    - Обрабатываются исключения: 400, 401, 403, 404, 500
    - Формат логов структурирован как JSON
    - Используется логгер 'cinemaapp'

--- cinemaapp/cinemaapp/middleware/logging_middleware.py
+++ cinemaapp/cinemaapp/middleware/logging_middleware.py
@@ -1,8 +1,10 @@
 import logging
 import json
 import time
+from django.core.exceptions import PermissionDenied
+from django.http import JsonResponse, Http404
 from django.utils.deprecation import MiddlewareMixin
-
+from rest_framework.exceptions import ValidationError as DRFValidationError, NotAuthenticated, AuthenticationFailed
 
 logger = logging.getLogger('cinemaapp')
 
@@ -10,6 +12,47 @@ logger = logging.getLogger('cinemaapp')
 class LoggingMiddleware(MiddlewareMixin):
     def process_request(self, request):
         request.start_time = time.time()
+
+
+    def process_exception(self, request, exception):
+        duration = round(time.time() - getattr(request,'start_time', time.time()), 3)
+
+        status_code = 500
+        message = "Внутренняя ошибка сервера"
+
+        if isinstance(exception,Http404):
+            status_code = 404
+            message = "Ресурс не найден"
+        elif isinstance(exception, PermissionDenied):
+            status_code = 403
+            message = "Доступ запрещен"
+        elif isinstance(exception,NotAuthenticated):
+            status_code = 401
+            message = "Требуется аутентификация"
+        elif isinstance(exception,AuthenticationFailed):
+            status_code = 401
+            message = "Ошибка аутентификация"
+        elif isinstance(exception, DRFValidationError):
+            status_code = 400
+            message = exception.detail
+            if isinstance(message,(dict,list)):
+                return JsonResponse(message,status=status_code)
+        else:
+            message = str(exception)
+
+
+        log_data = {
+                "timestamp": time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime()),
+                "level": "error",
+                "message": str(exception),
+                "method": request.method,
+                "path": request.get_full_path(),
+                "statusCode": status_code,
+                "duration": duration,
+        }
+
+        logger.error(json.dumps(log_data))
+        return JsonResponse({"error": message}, status=status_code)
 
 
     def process_response(self, request, response):
